20d490521d6bc5c783b203533d5937ca4f7d0023
jeonggu <dev.jeonggu@gmail.com>
2025-01-15 23:52:51 +0900

    feat: add isAdmin and nickname fields to JwtUserDetails
    - Added isAdmin and nickname fields to JwtUserDetails

--- src/main/java/com/board/entity/auth/JwtUserDetails.java
+++ src/main/java/com/board/entity/auth/JwtUserDetails.java
@@ -3,17 +3,28 @@ package com.board.entity.auth;
 public class JwtUserDetails {
     private final Long userId;
     private final String email;
+    private final String isAdmin;
+    private final String nickname;
 
-    public JwtUserDetails(Long userId, String email) {
+    public JwtUserDetails(Long userId, String email, String isAdmin, String nickname) {
         this.userId = userId;
         this.email = email;
+        this.isAdmin = isAdmin;
+        this.nickname = nickname;
     }
 
-    public Long getId() {  // `id` 이름으로 반환하도록 메서드 추가
+    public Long getId() {
         return userId;
     }
 
     public String getEmail() {
         return email;
+    }
+    
+    public String getIsAdmin() {
+        return isAdmin;
+    }
+    public String getNickname() {
+        return nickname;
     }
 }
