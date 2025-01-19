a0a6ef3e2f0f9a4c1f99a8b88e533f169fb289b1
jeonggu <dev.jeonggu@gmail.com>
2025-01-19 23:24:35 +0900

    fix: update auth logic
    - change return data format
    - add password encryption logic

--- src/main/java/com/board/web/controller/auth/AuthController.java
+++ src/main/java/com/board/web/controller/auth/AuthController.java
@@ -6,6 +6,7 @@ import com.board.dto.BoardResponse;
 import com.board.entity.Board;
 import com.board.service.BoardService;
 import com.board.service.auth.AuthService;
+import com.board.utils.ResponseUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +16,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,7 +37,17 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
+    @GetMapping
+    public ResponseEntity<?> checkAuthentication() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body(Map.of("authenticated", false, "message", "Unauthorized"));
+        }
+
+        return ResponseEntity.ok(Map.of("authenticated", true));
+    }
+    
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> loginRequest) {
         String email = loginRequest.get("email");
@@ -42,17 +55,14 @@ public class AuthController {
 
         try {
             Map<String, Object> response = authService.login(email, password);
-
+            
             if ((boolean) response.get("success")) {
-                return ResponseEntity.ok(response);
+                return ResponseEntity.ok(ResponseUtil.successResponse(response));
             } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ResponseUtil.errorResponse("서버 오류가 발생했습니다."));
             }
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
-                "success", false,
-                "message", "서버 오류가 발생했습니다."
-            ));
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ResponseUtil.errorResponse("서버에 오류가 발생했습니다."));
         }
     }
 }
